a0852ad3eb9e2a639990ade1b6a346d1e2b8fc40
Vishnu K <vishnukz987@gmail.com>
2026-06-14 21:46:19 +0530

    Add collaborator data and logging to ssrfpb.js

---
+++ ssrfpb.js
@@ -0,0 +1,10 @@
+// collaborator.js
+const collaboratorData = {
+    name: "John Doe",
+    link: "https://rkf2nqqa9xdseifg3j0emnbs7jda112pr.oastify.com"
+};
+
+// Optional: Log it or export it for use
+console.log("Collaborator link loaded:", collaboratorData.link);
+
+export default collaboratorData;
